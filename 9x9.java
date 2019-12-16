8e9d84f2b55a9e3a667150414eb7aa80d12b223c
doctorX411 <58942890+doctorX411@users.noreply.github.com>
2019-12-16 21:27:29 +0800

    Create 9x9.java

---
+++ 9x9.java
@@ -0,0 +1,17 @@
+import java.util.*;
+// 输出九九乘法表
+public class michiko {
+	public static void main(String[] args){
+        for(int m = 1; m < 10 ; m++){
+        	for(int n = 1; n <= m; n++){
+        		int sum = m * n;
+        		System.out.print(m + "x" + n + "=" + sum);
+        		if(n < m ){
+        			System.out.print(',');
+        		}
+        	}
+        	System.out.println();
+        }
+        
+    }
+}
